4ca7ead15f42ba48622613baee1b824831083473
Alan Lindao <alan@internit.com.br>
2021-02-28 11:07:09 -0300

    Adicionando Primeiro Programa em Java

---
+++ Primeiro Programa/Tabuada.java
@@ -0,0 +1,53 @@
+/* 
+Nome: Alan Azeredo Guedes
+Matricula: Botar Matricula aqui?, não rola ...
+*/
+
+import java.util.Scanner;
+
+  class Tabuada {
+
+    public static void main(String args[]){
+
+        System.out.println("##########################");
+        System.out.println("###  Programa Tabuada  ###");
+        System.out.println("##########################");
+
+        Scanner ler = new Scanner(System.in);
+
+        int i = 1;
+        while(i == 1){
+            
+            System.out.printf("\n##########################\n");
+            System.out.printf("Numero para a Tabuada: ");
+            int valor = ler.nextInt();
+            System.out.printf("##########################\n");
+
+            tabuada(valor);
+
+            System.out.printf("##########################\n");
+            System.out.print("Digite 1 para continuar: ");
+            i = ler.nextInt();
+            System.out.printf("##########################\n");
+
+        }
+        
+    
+    }
+
+    private static void tabuada(int valor){
+        System.out.printf("\n##########################\n");
+        System.out.printf("Resultado da Tabuada de %d \n", valor);
+        System.out.printf("##########################\n");
+
+        int i = 0;
+        while (i <= 10){
+            int resultado = i * valor;
+            System.out.printf("%d x %d = %d \n",i,valor, resultado);
+            i += 1;
+        }
+    }
+
+
+
+}
