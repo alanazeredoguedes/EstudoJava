e1a3fe4996510a58e8120151515d353857f7bdc2
Alan Guedes <alanguedes00@live.com>
2021-03-10 10:38:53 -0300

    Adicionando Exercicio 2

---
+++ Exercicio2/AlanTabuada.java
@@ -0,0 +1,53 @@
+/* 
+Nome: Alan Azeredo Guedes
+Matricula: Botar Matricula aqui?, não rola ...
+*/
+
+import java.util.Scanner;
+
+  class AlanTabuada {
+
+    public static void main(String args[]){
+
+        System.out.println("##########################");
+        System.out.println("###  Programa Tabuada  ###");
+        System.out.println("##########################");
+
+        Scanner ler = new Scanner(System.in);
+
+        int i = 1;
+        while(i == 1){
+            
+            System.out.printf("\n##########################\n");
+            System.out.printf("Olá Alan, entre com um numero: ");
+            int valor = ler.nextInt();
+            System.out.printf("##########################\n");
+
+            tabuada(valor);
+
+            System.out.printf("##########################\n");
+            System.out.print("Digite 1 para continuar: ");
+            i = ler.nextInt();
+            System.out.printf("##########################\n");
+
+        }
+        
+    
+    }
+
+    private static void tabuada(int valor){
+        System.out.printf("\n##########################\n");
+        System.out.printf("Tabuada de Somar de %d \n", valor);
+        System.out.printf("##########################\n"); 
+
+        int i = 0;
+        while (i <= 10){
+            int resultado = i + valor;
+            System.out.printf("%d + %d = %d \n",valor,i, resultado);
+            i += 1;
+        }
+    }
+
+
+
+}
