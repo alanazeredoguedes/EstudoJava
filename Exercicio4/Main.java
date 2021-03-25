a232af4ca95474d2e2a2fa7117bc33a34617c8db
Alan Guedes <alanguedes00@live.com>
2021-03-24 21:08:50 -0300

    Add Exercicio 4

---
+++ Exercicio4/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Professor professor = new Professor("POO",2000.00f);
+
+        professor.setNome("Lucas Souza");
+        professor.fazerNiver();
+
+    }
+
+
+
+}
