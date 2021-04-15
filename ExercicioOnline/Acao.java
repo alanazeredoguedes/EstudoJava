ac1d5ea37bb4c4768f1313d74809c68268b127e2
Alan Guedes <alanguedes00@live.com>
2021-04-15 20:29:16 -0300

    Adicionanado Files

---
+++ ExercicioOnline/Acao.java
@@ -0,0 +1,34 @@
+package ExercicioOnline;
+
+public class Acao {
+
+    private String nome;
+
+    private float preco;
+
+    private float valorizacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getValorizacao() {
+        return valorizacao;
+    }
+
+    public void setValorizacao(float valorizacao) {
+        this.valorizacao = valorizacao;
+    }
+}
